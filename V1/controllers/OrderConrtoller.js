a37f4fadfcc8e8fdfb19bfc98168a7f7c0b0ddd0
Illuminz-TUSHAR <tushar@illuminz.com>
2023-02-15 18:26:34 +0530

    Item Qunatity

--- V1/controllers/OrderConrtoller.js
+++ V1/controllers/OrderConrtoller.js
@@ -192,7 +192,7 @@ class Order {
 
             for (let item of items) {
                 
-                itemDetails = await itemService.getItem({ _id: item.itemId })
+                itemDetails = await itemService.getItem({ _id: item.itemId, isDeleted: false })
                 if (itemDetails.quantity >= item.quantity) {
                     itemFilterArr.push(item)
                     sum += (item.quantity * item.price);
